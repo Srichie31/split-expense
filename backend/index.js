88a7558af5306582a5ac76c6b38660020ba66bc7
Srichie31 <sreenath.ab11@gmail.com>
2025-06-16 14:26:44 +0530

    added cors

--- backend/index.js
+++ backend/index.js
@@ -9,7 +9,10 @@ const groupRoutes = require("./routes/group");
 const expenseRoutes = require("./routes/expense");
 const userRoutes = require("./routes/user");
 const verifyToken = require("./middleware/verifyToken");
-app.use(cors());
+app.use(cors({
+  origin: 'http://localhost:4200',
+  credentials: true,
+}));
 app.use(express.json());
 
 app.use("/auth", authRoutes);
