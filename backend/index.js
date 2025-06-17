05e33fd41d63e9f0910217e9eb79d17e52bcc327
Srichie31 <sreenath.ab11@gmail.com>
2025-06-17 07:24:51 +0530

    cors issue

--- backend/index.js
+++ backend/index.js
@@ -21,8 +21,6 @@ app.use("/groups", groupRoutes);
 app.use("/expenses", expenseRoutes);
 app.use("/users", userRoutes);
 
-console.log("hi");
-
 mongoose
   .connect(process.env.MONGO_URI, {
     useNewUrlParser: true,
@@ -31,8 +29,8 @@ mongoose
   .then(() => console.log("Connected to MongoDB"))
   .catch((err) => console.error("MongoDB error:", err));
 
-// const port = 3000;
-// app.listen(port, () => {
-//   console.log(`Server running on port ${port}`);
-// });
+const port = 3000;
+app.listen(port, () => {
+  console.log(`Server running on port ${port}`);
+});
 
